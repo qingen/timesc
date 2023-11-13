ab9fb4079b7f18789d2722a6485ab21c67637bc5
qingen <qingenz123@126.com>
2023-11-13 15:49:28 +0800

    update  for train pipeline

--- tc_fft_train_occur.py
+++ tc_fft_train_occur.py
@@ -5004,9 +5004,9 @@ def augment_bad_data_add_credit_relabel_multiclass_train_occur_continue_for_repo
     filter_num_ratio = 1 / 8  # 1/5
     ftr_good_year_split = 2017
     ########## model
-    epochs = 2  # 20  10
+    epochs = 1  # 20  10
     patiences = 1  # 10  5
-    kernelsize = 8  # 16
+    kernelsize = 4  # 16
     cluster_model_path = './model/cluster_step' + str(step) + '_credit1_90_'+str(ftr_good_year_split)+ '_'+date_str +'/'
     cluster_model_file = date_str + '-repr-cluster-partial-train-6.pkl'
     cluster_less_train_num = 800    # 200
@@ -6215,7 +6215,7 @@ def augment_bad_data_add_credit_relabel_multiclass_augment_ftr_select_train_occu
     num_leaves = 3 # 3 7
     n_estimators = 50 # 50 100
     class_weight =  None # 'balanced'  None
-    fdr_level = 0.05 # 0.05(default)  0.04 0.03 0.02 0.01 0.001 0.0001 0.00001
+    fdr_level = 0.0001 # 0.05(default)  0.04 0.03 0.02 0.01 0.001 0.0001 0.00001
     cluster_model_path = './model/cluster_step' + str(step) + '_credit1_90_'+str(ftr_good_year_split)+ '_'+date_str +'/'
     cluster_model_file = date_str + '-repr-cluster-partial-train-6.pkl'
     cluster_less_train_num = 800    # 200
@@ -6788,13 +6788,13 @@ def ensemble_data_augment_group_ts_dl_ftr_select_nts_ml_base_score():
     ########## model
     epochs = 2    # 3
     patiences = 1  # 2
-    kernelsize = 16
-    max_depth = 3 # 2 3  -1
-    num_leaves = 7 # 3 7  31
-    n_estimators = 100 #  50 100 100
-    class_weight =  'balanced' # None 'balanced' None
-    fdr_level = 0.05  # 0.05(default)  0.04 0.03 0.02 0.01 0.001 0.0001 0.00001
-    lc_c = [0.1, 0.02, 0.2,] #
+    kernelsize = 4
+    max_depth = 2 # 2 3  -1
+    num_leaves = 3 # 3 7  31
+    n_estimators = 50 #  50 100 100
+    class_weight =  None # None 'balanced' None
+    fdr_level = 0.001  # 0.05(default)  0.04 0.03 0.02 0.01 0.001 0.0001 0.00001
+    lc_c = [0.06, 0.03, 2.0,] #
     cluster_less_train_num = 800
     cluster_less_val_num = 200
     cluster_less_test_num = 100
@@ -6919,7 +6919,7 @@ if __name__ == '__main__':
     # ts2vec_relabel()
     # augment_bad_data_relabel_train_occur_continue_for_report()
     # augment_bad_data_relabel_multiclass_train_occur_continue_for_report()
-    augment_bad_data_add_credit_relabel_multiclass_train_occur_continue_for_report()
+    # augment_bad_data_add_credit_relabel_multiclass_train_occur_continue_for_report()
     # tsfresh_test()
     # augment_bad_data_add_credit_relabel_multiclass_augment_ftr_select_train_occur_continue_for_report()
-    # ensemble_data_augment_group_ts_dl_ftr_select_nts_ml_base_score()
+    ensemble_data_augment_group_ts_dl_ftr_select_nts_ml_base_score()
