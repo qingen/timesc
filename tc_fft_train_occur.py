123caa6e5d6313df5783bbcef12670908b9f1fd1
qingen <qingenz123@126.com>
2023-11-24 19:41:02 +0800

    add optuna test

--- tc_fft_train_occur.py
+++ tc_fft_train_occur.py
@@ -7668,11 +7668,79 @@ def multiple_hypothesis_testing():
             ml_model_forward_ks_roc(model_file_path, result_file_path, df_test_ftr_select_notime.loc[:,select_cols], np.array(df_test_ftr_select_notime.loc[:,'Y']),
                                  np.array(df_test_ftr_select_notime.loc[:,'CUSTOMER_ID']))
 import optuna
-def objective(trial):
+def objectives(trial):
     x = trial.suggest_uniform('x', -10, 10)
     y = trial.suggest_uniform('y', -10, 10)
     return (x + y) ** 2
 
+import numpy as np
+import optuna
+from optuna.integration import CatBoostPruningCallback
+
+import catboost as cb
+from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+
+def objective(trial: optuna.Trial) -> float:
+    data, target = load_breast_cancer(return_X_y=True)
+    train_x, valid_x, train_y, valid_y = train_test_split(data, target, test_size=0.25)
+
+    param = {
+        "objective": trial.suggest_categorical("objective", ["Logloss", "CrossEntropy"]),
+        "colsample_bylevel": trial.suggest_float("colsample_bylevel", 0.01, 0.1, log=True),
+        "depth": trial.suggest_int("depth", 1, 12),
+        "boosting_type": trial.suggest_categorical("boosting_type", ["Ordered", "Plain"]),
+        "bootstrap_type": trial.suggest_categorical(
+            "bootstrap_type", ["Bayesian", "Bernoulli", "MVS"]
+        ),
+        "used_ram_limit": "3gb",
+        "eval_metric": "Accuracy",
+    }
+
+    if param["bootstrap_type"] == "Bayesian":
+        param["bagging_temperature"] = trial.suggest_float("bagging_temperature", 0, 10)
+    elif param["bootstrap_type"] == "Bernoulli":
+        param["subsample"] = trial.suggest_float("subsample", 0.1, 1, log=True)
+
+    gbm = cb.CatBoostClassifier(**param)
+
+    pruning_callback = CatBoostPruningCallback(trial, "Accuracy")
+    gbm.fit(
+        train_x,
+        train_y,
+        eval_set=[(valid_x, valid_y)],
+        verbose=0,
+        early_stopping_rounds=100,
+        callbacks=[pruning_callback],
+    )
+
+    # evoke pruning manually.
+    pruning_callback.check_pruned()
+
+    preds = gbm.predict(valid_x)
+    pred_labels = np.rint(preds)
+    accuracy = accuracy_score(valid_y, pred_labels)
+
+    return accuracy
+def opt_test():
+    study = optuna.create_study(
+        pruner=optuna.pruners.MedianPruner(n_warmup_steps=5), direction="maximize"
+    )
+    study.optimize(objective, n_trials=100, timeout=600)
+
+    print("Number of finished trials: {}".format(len(study.trials)))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: {}".format(trial.value))
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
+        
 def optuna_test():
     study = optuna.create_study(study_name='test',direction='maximize',
                                 storage='sqlite:///db.sqlite3',load_if_exists=True)
